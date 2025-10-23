91c0daa09a435bf47d5f4b5b42cc0fe769078cb4
Shasikumar10 <shashikumar44887@gmail.com>
2025-10-23 21:37:04 +0530

    updated index.js and app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import { AuthProvider } from './contexts/AuthContext';
+import ProtectedRoute from './components/common/ProtectedRoute';
+import AdminRoute from './components/common/AdminRoute';
+import Navbar from './components/common/Navbar';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import ReportItem from './pages/ReportItem';
+import BrowseItems from './pages/BrowseItems';
+import ItemDetail from './pages/ItemDetail';
+import Claims from './pages/Claims';
+import MyItems from './pages/MyItems';
+import Profile from './pages/Profile';
+import AdminDashboard from './pages/AdminDashboard';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthProvider>
+      <Router>
+        <div className="app">
+          <Navbar />
+          <main className="main-content">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/browse" element={<BrowseItems />} />
+              <Route path="/items/:id" element={<ItemDetail />} />
+              
+              {/* Protected Routes */}
+              <Route path="/report" element={
+                <ProtectedRoute>
+                  <ReportItem />
+                </ProtectedRoute>
+              } />
+              <Route path="/claims" element={
+                <ProtectedRoute>
+                  <Claims />
+                </ProtectedRoute>
+              } />
+              <Route path="/my-items" element={
+                <ProtectedRoute>
+                  <MyItems />
+                </ProtectedRoute>
+              } />
+              <Route path="/profile" element={
+                <ProtectedRoute>
+                  <Profile />
+                </ProtectedRoute>
+              } />
+              
+              {/* Admin Routes */}
+              <Route path="/admin/*" element={
+                <AdminRoute>
+                  <AdminDashboard />
+                </AdminRoute>
+              } />
+              
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </main>
+          <Toaster position="top-right" />
+        </div>
+      </Router>
+    </AuthProvider>
   );
 }
 
